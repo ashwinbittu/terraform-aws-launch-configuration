72c3648e1c4477400fd4ae13b6cc063223257262
ashwinbittu <ashwin.bittu@gmail.com>
2022-07-10 10:23:19 +1000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,50 @@
+data "aws_ami" "repave_images" {
+  #most_recent = true
+  #filter {
+  #  name   = "name"
+  #  values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  #}
+  #filter {
+  #  name   = "virtualization-type"
+  #  values = ["hvm"]
+  #}
+  #owners = ["099720109477"] # Canonical
+    
+  filter {
+    name   = "name"
+    values = ["repave-ami-${var.app_version}.0"]
+  }
+  owners = [var.ami_owners] # Canonical
+  
+  #filter {
+  #  name   = "name"
+  #  values = ["centtestami"]
+  #}
+  #owners = ["043042377913"] # Canonical  
+}
+
+resource "aws_launch_configuration" "hatest" {
+  name_prefix     = "hatest-" 
+  image_id        = data.aws_ami.repave_images.id
+  instance_type   = var.inst_type
+  key_name        = var.aws_ec2_keypair_name
+  security_groups = var.aws_security_group_instances_id 
+  #user_data = data.template_cloudinit_config.cloudinit-vol.rendered
+
+  root_block_device {
+      volume_size           = "10"
+      volume_type           = "gp2"
+      delete_on_termination = true
+    }
+ 
+  ebs_block_device {
+      device_name           = var.inst_device_name
+      snapshot_id           = var.aws_ebs_snap_id == "" ? "" : var.aws_ebs_snap_id
+      volume_type           = var.aws_ebs_volume_type == "" ? "gp2" : var.aws_ebs_volume_type
+      volume_size           = var.aws_ebs_volume_size == "" ? "10" : var.aws_ebs_volume_size
+      #delete_on_termination = false
+    }  
+  lifecycle {
+    create_before_destroy = true
+  } 
+}
